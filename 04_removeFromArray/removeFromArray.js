41bfb775192a1a818a84fa08248d0c5548b69bea
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2024-09-17 23:22:53 -0500

    Finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i];
+        
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    }
+    
+    return newArray;
+}
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 4, 9, 2, 1));
 // Do not edit below this line
 module.exports = removeFromArray;
